27e7fc0df57464e2f4d090305d384a835f4fb4f2
Lee Faus <lee.faus@armory.io>
2019-07-22 16:13:01 -0400

    add unit test

--- src/test/kotlin/io/armory/services/person/PersonControllerTest.kt
+++ src/test/kotlin/io/armory/services/person/PersonControllerTest.kt
@@ -1,10 +1,31 @@
 package io.armory.services.person
 
+<<<<<<< HEAD
 import org.junit.jupiter.api.Assertions.*
 
 internal class PersonControllerTest {
 
     @org.junit.jupiter.api.Test
     fun greeting() {
+=======
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+internal class PersonControllerTest {
+
+    val personController = PersonController()
+
+    @Test
+    fun greetingWithValue() {
+        val result = personController.greeting(name = "Armory")
+        assertNotNull(result)
+        assertEquals("Hello, Armory", result.content )
+>>>>>>> add unit test
     }
 }
