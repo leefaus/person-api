8173bc436a27f6eafba0e9c98664c97844517d9f
Lee Faus <lee.faus@armory.io>
2019-07-22 16:13:01 -0400

    add unit test

---
+++ src/test/kotlin/io/armory/services/person/PersonControllerTest.kt
@@ -0,0 +1,10 @@
+package io.armory.services.person
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class PersonControllerTest {
+
+    @org.junit.jupiter.api.Test
+    fun greeting() {
+    }
+}
